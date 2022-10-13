63a453676dab337531da0a747492f92f39fac584
John Kim Alcantara <114913352+JkAlcntr@users.noreply.github.com>
2022-10-13 12:33:53 +0800

    Add files via upload

--- static/js/main.js
+++ static/js/main.js
@@ -92,9 +92,6 @@ const sr = ScrollReveal({
 
 sr.reveal(`.home__data, .home__img,
             .about__data, .about__img,
-            .services__content, .menu__content,
-            .app__data, .app__img,
-            .contact__data, .contact__button,
             .footer__content`, {
     interval: 200
 })
